2783981ed8321ddfac1ea0eb5cface8f01e20599
The King Lion <kinglionkl250@gmail.com>
2024-06-09 16:13:17 -0400

    Updated Category.js

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,10 +1,27 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash, faEdit } from '@fortawesome/free-solid-svg-icons';
+import Modal from './Modal';
 import '../App.css';
 
 const Category = ({ categoryName, color, videos }) => {
+    const [showModal, setShowModal] = useState(false);
+    const [selectedVideo, setSelectedVideo] = useState(null);
+
+    const handleEdit = (video) => {
+        setSelectedVideo(video);
+        setShowModal(true);
+    };
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+        setSelectedVideo(null);
+    }
+
+    const handleSave = (updatedVideo) => {
+        console.log('Video actualizado:', updatedVideo);
+    }
+
     return (
         <section className='category'>
             <div className='category-header' style={{ backgroundColor: color }}>
@@ -13,9 +30,9 @@ const Category = ({ categoryName, color, videos }) => {
             <div className='videos'>
                 {videos.map((video, index) => (
                     <div className='card' key={index} style={{ borderColor: color }}>
-                        <img src={video.thumbnail} alt={video.title} className='video-thumbnail' />
+                        <img src={video.thumbnail} alt={video.title} />
                         <div className='card-buttons' style={{ borderTopColor: color }}>
-                            <button className='edit-button'>
+                            <button className='edit-button' onClick={() => handleEdit(video)}>
                                 <FontAwesomeIcon icon={faEdit} /> Editar
                             </button>
                             <button className='delete-button'>
@@ -25,6 +42,7 @@ const Category = ({ categoryName, color, videos }) => {
                     </div>
                 ))}
             </div>
+            <Modal show={showModal} onClose={handleCloseModal} videoData={selectedVideo} onSave={handleSave} />
         </section>
     );
 };
